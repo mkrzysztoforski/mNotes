16375c7a2c0aff256d9cb76ec4d90e5612ae19df
Maciej Krzysztoforski <maciek@MacBook-Pro-Maciej.local>
2020-11-10 11:53:44 +0100

    docker

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ResteX } from '@reste/x';
+import express from 'express';
 import { HomeController } from './controllers/HomeController';
 import { NoteController } from './controllers/NoteController';
 
-const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
 ResteX.createApp(app, [HomeController, NoteController]);
 
-app.listen(8080, () => console.log('Server started at port 8080'));
+app.listen(port, () => console.log(`Server started at port ${port}`));
 
 app.on('error', (error: any) => console.log('Error:', error));
